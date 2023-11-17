123b5a7734880a6e0202d4d0dccf1843450d1584
aks1374 <150404553+aks1374@users.noreply.github.com>
2023-11-17 18:11:23 +0330

    Create db.php

---
+++ note/db.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$conn = mysqli_connect(
+  'adhamsaadi21.mysql.pythonanywhere-services.com',
+  'adhamsaadi21',
+  'new_pass_23',
+  'adhamsaadi21$default'
+) or die(mysqli_erro($mysqli));
+
+?>
