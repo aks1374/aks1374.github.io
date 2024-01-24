ab343e2319eade5ccfbd62d72e084833eb9fe1b0
aks1374 <150404553+aks1374@users.noreply.github.com>
2024-01-24 17:49:17 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
+// Initialize the Telegram object
+Telegram.init({
+    // Specify your bot username or deep linking parameters
+    // based on your application's requirements
+    bot_username: "zelyxa_bot",
+    // ... Other options
+});
+
+// Listen for the 'tgWebAuth' event to retrieve the user's chat ID
+window.addEventListener("tgWebAuth", function (e) {
+    const chatId = e.detail.user_id;
+    console.log("Chat ID:", chatId);
+    sendMarket(chatId); // Call the sendMarket function with the chat ID
+});
+
 function sendRequest(command, data) {
-    const serverUrl = 'https://frank-epic-sponge.ngrok-free.app/bot-command';
+    const serverUrl = "https://frank-epic-sponge.ngrok-free.app/bot-command";
     const payload = {
         command: command,
-        data: data
+        ...data,
     };
 
     fetch(serverUrl, {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(payload),
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Server Response:', data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Server Response:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
 }
 
-function sendMarket() {
-    const chatId = tg.getChat().id;
-    const symbolSelect = document.getElementById('m_symbolSelect');
+function sendMarket(chatId) {
+    const symbolSelect = document.getElementById("m_symbolSelect");
     const m_symbol = symbolSelect.value;
     const data = {
         chatId: chatId,
         m_symbol: m_symbol,
     };
 
-    sendRequest('/market', data);
-    Swal.fire('Sent!', 'Your data has been sent.', 'success');
+    sendRequest("/market", data);
+    Swal.fire("Sent!", "Your data has been sent.", "success");
 }
