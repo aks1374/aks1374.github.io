e38c9503ea168bf5380328abebc01b7b0d5c9299
aks1374 <150404553+aks1374@users.noreply.github.com>
2024-01-24 20:12:13 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,39 @@
-// Initialize the Telegram object
-Telegram.init({
-    // Specify your bot username or deep linking parameters
-    // based on your application's requirements
-    bot_username: "zelyxa_bot",
-    // ... Other options
-});
-
-// Listen for the 'tgWebAuth' event to retrieve the user's chat ID
-window.addEventListener("tgWebAuth", function (e) {
-    const chatId = e.detail.user_id;
-    console.log("Chat ID:", chatId);
-    sendMarket(chatId); // Call the sendMarket function with the chat ID
-});
-
-function sendRequest(command, data) {
-    const serverUrl = "https://frank-epic-sponge.ngrok-free.app/bot-command";
-    const payload = {
-        command: command,
-        payload: data, // Change this line to include the data in a 'payload' property
-    };
-
-    fetch(serverUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("Server Response:", data);
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-        });
+function getChatId() {
+  // Retrieve the chat ID from the user input (e.g., input field, dropdown, etc.)
+  const chatIdInput = document.getElementById('chatIdInput');
+  return chatIdInput.value;
 }
+function sendRequest(command, data) {
+  const serverUrl = 'https://frank-epic-sponge.ngrok-free.app/bot-command';
+  const chatId = getChatId(); // Obtain the chat ID from the user input
+  const payload = {
+    command: command,
+    chat_id: chatId, // Include the chat ID in the payload
+    ...data
+  };
 
-function sendMarket(chatId) {
-    const symbolSelect = document.getElementById("m_symbolSelect");
-    const m_symbol = symbolSelect.value;
-    const data = {
-        chatId: chatId,
-        m_symbol: m_symbol,
-    };
-
-    sendRequest("/market", data);
-    Swal.fire("Sent!", "Your data has been sent.", "success");
+  fetch(serverUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Server Response:', data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+function sendMarket() {
+  const symbolSelect = document.getElementById('m_symbolSelect');
+  const m_symbol = symbolSelect.value;
+  const data = {
+    m_symbol: m_symbol,
+  };
+  
+  sendRequest('/market', data);
+  Swal.fire('Sent!', 'Your data has been sent.', 'success');
 }
